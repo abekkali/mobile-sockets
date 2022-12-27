67821df24174fea815ba7c2266c85a6b0edfe4cb
anass <anasb@DESKTOP-8ERSIGR>
2022-12-27 13:42:33 +0100

    Ajoutez des fichiers projet.

---
+++ App4/App4/Views/AboutPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        public void envoyer(object sender, EventArgs e)
+        {
+            var ad = adresse.Text;
+            var p = port.Text;
+            var m = message.Text;
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //IPEndPoint ip = new IPEndPoint(ad, 9999);
+            clientSocket.Connect(ad,9999);
+           Console.WriteLine($"le message est : {m}");
+           var bytesToSend = Encoding.UTF8.GetBytes(m);
+           clientSocket.Send(bytesToSend);
+           
+
+           //clientSocket.Close();
+        }
+    }
+}
